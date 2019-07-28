a54afbd0c123bddfd7697455afbd372ccb9a6874
vmuser <vmuser@ubuntu-phd.c.scientific-air-129516.internal>
2019-07-28 12:11:19 -0700

    successful output to mysql

--- src/main/java/jdes/gdeltedu/FullDataOutput.java
+++ src/main/java/jdes/gdeltedu/FullDataOutput.java
@@ -9,7 +9,7 @@ import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.DataTypes;
 
 import scala.Tuple2;
-
+import java.util.Properties;
 
 
 public class FullDataOutput {
@@ -24,65 +24,81 @@ public class FullDataOutput {
 			System.out.println("Error with commandline inputs!");
 			return;
 		}
-		
-		
-		
+
 		SparkSession spark = SparkSession.builder().master("local").appName("gdelt-education-output").
 				config("some config", "value").getOrCreate();
 		// option("inferSchema", "false")
 		Dataset<Row> inputdf = spark.read().format("com.databricks.spark.avro").load(inputDir);
 		inputdf.createOrReplaceTempView("gdeltedu");
 
-		// CONCAT(GLOBALEVENTID, SQLDATE, EventCode, ActionGeo_ADM1Code) as RowKey,
-		Dataset<Row> df = inputdf.sqlContext().sql("Select "
-				+ "GLOBALEVENTID, Year, FractionDate, Actor1Code, Date, DateAdded, " + 
-				"Actor1Name, Actor1CountryCode, Actor1KnownGroupCode, Actor1Religion1Code, " + 
-				"Actor1Type1Code, Actor2Type2Code, Actor1Type3Code, Actor2Code, Actor2Name, " + 
-				"IsRootEvent, CAST(EventCode AS STRING) EventCode, CAST(EventBaseCode AS STRING) as EventBaseCode,"
-				+ " CAST(EventRootCode AS STRING) AS EventRootCode, QuadClass, NumMentions, " + 
-				"AvgTone,Actor1Geo_Type, Actor1Geo_FullName, ActionGeo_Type, ActionGeo_FullName, " + 
-				"ActionGeo_CountryCode, ActionGeo_ADM1Code, ActionGeo_Lat, ActionGeo_Long, " + 
-				"SOURCEURL from gdeltedu ORDER BY DATEADDED");
-		
-		df.createOrReplaceTempView("gdeltViaAvro");
-		
 		//  rowkey if I decide to use HBase CONCAT(GLOBALEVENTID, Date, EventCode, NumMentions) as RowKey
 		
-//		Dataset<Row> gdeltFreqUsed = inputdf.sqlContext().sql("Select "
-//				+ " GLOBALEVENTID, Year, Actor1Code, Date, DateAdded, " + 
-//				"Actor1Name, Actor1Code, " + 
-//				" Actor2Code, Actor2Name, " + 
-//				"IsRootEvent, EventCode,  QuadClass, NumMentions, " + 
-//				"AvgTone, ActionGeo_FullName, " + 
-//				"ActionGeo_CountryCode, ActionGeo_ADM1Code,  " + 
-//				"SOURCEURL from gdeltedu ORDER BY DateAdded");
+		Dataset<Row> gdeltFreqUsed = inputdf.sqlContext().sql("Select "
+				+ " GLOBALEVENTID, Year, Actor1Code, Date, DateAdded, " + 
+				"Actor1Name, " + 
+				" Actor2Code, Actor2Name, " + 
+				"IsRootEvent, EventCode,  QuadClass, NumMentions, " + 
+				"AvgTone, ActionGeo_FullName, " + 
+				"ActionGeo_CountryCode, ActionGeo_ADM1Code,  " + 
+				"SOURCEURL from gdeltedu ORDER BY DateAdded");
+		
+		String table = "gdelt.freqused";
+		String lessFreqTable = "gdelt.lessfreqused";
+		Properties connectionProperties = new Properties();
+		connectionProperties.put("user", "root");
+		connectionProperties.put("password", "");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		
+		gdeltFreqUsed.write().mode(SaveMode.Overwrite).jdbc("jdbc:mysql://localhost:3306/gdelt", table, connectionProperties);
+		
+		Dataset<Row> gdeltLessFreqUsed = inputdf.sqlContext().sql("Select GLOBALEVENTID, FractionDate, "
+				+ "Actor1CountryCode, Actor1KnownGroupCode, Actor1Religion1Code, "
+				+ "Actor1Type1Code, Actor1Type3Code,  Actor2Type2Code, "
+				+ "EventBaseCode, EventRootCode, Actor1Geo_Type, Actor1Geo_FullName, ActionGeo_Type, "
+				+ "ActionGeo_Lat, ActionGeo_Long from gdeltedu ORDER BY FractionDate");
+		
+		
+		gdeltLessFreqUsed.write().mode(SaveMode.Overwrite).jdbc("jdbc:mysql://localhost:3306/gdelt", lessFreqTable, connectionProperties);
+		
+//		df.show();
 //		
-//		Dataset<Row> gdeltLessFreqUsed = inputdf.sqlContext().sql("Select GLOBALEVENTID, FractionDate, "
-//				+ "Actor1CountryCode, Actor1KnownGroupCode, Actor1Religion1Code, "
-//				+ "Actor1Type1Code, Actor2Type2Code, Actor1Type3Code, "
-//				+ "EventBaseCode, EventRootCode, Actor1Geo_Type, Actor1Geo_FullName, ActionGeo_Type, "
-//				+ "ActionGeo_Lat, ActionGeo_Long from gdeltedu ORDER BY FractionDate");
-		
-		
-		df.show();
-		
-		dataTypePrint(df);
-//		dataTypePrint(gdeltLessFreqUsed);
-		
+//		dataTypePrint(df);
+//		dataTypePrint(gdeltLessFreqUsed);	
 //		Dataset<Row> df3 = df.withColumn("RowKey", df.col(""+"SQLDATE"+"Actor1Name"+""+""));
-		
-
 //		df2.coalesce(1).write().format("com.databricks.spark.avro").mode(SaveMode.Overwrite).save(output);
 //		df2.coalesce(1).write().mode(SaveMode.Overwrite).csv(output);
 		
-		
-		
-		
+
 		
 	}
 	
-	
-	
+//	public static void connectToHBase() {
+//		TableName tableName = TableName.valueOf("stock-prices");
+//		 
+//	    Configuration conf = HBaseConfiguration.create();
+//	    conf.set("hbase.zookeeper.property.clientPort", "2181");
+//	    conf.set("hbase.zookeeper.quorum", "sandbox.hortonworks.com");
+//	    conf.set("zookeeper.znode.parent", "/hbase-unsecure");
+//	    Connection conn = ConnectionFactory.createConnection(conf);
+//	    Admin admin = conn.getAdmin();
+//	    if (!admin.tableExists(tableName)) {
+//	        admin.createTable(new HTableDescriptor(tableName).addFamily(new HColumnDescriptor("cf")));
+//	    }
+//	 
+//	    Table table = conn.getTable(tableName);
+//	    Put p = new Put(Bytes.toBytes("AAPL10232015"));
+//	    p.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("close"), Bytes.toBytes(119));
+//	    table.put(p);
+//	 
+//	    Result r = table.get(new Get(Bytes.toBytes("AAPL10232015")));
+//	    System.out.println(r);
+//	}
+
 	
 	public static void dataTypePrint(Dataset<Row> dataset) {
 		Tuple2<String, String>[] tuples = dataset.dtypes();
