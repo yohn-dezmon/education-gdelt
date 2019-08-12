a40e8765d5600bf7467d379b1a9c48a51fa06cf2
vmuser <vmuser@ubuntu-phd.c.scientific-air-129516.internal>
2019-08-12 11:26:53 -0700

    success mysql-flask

---
+++ gdelt-edu-web/db.py
@@ -0,0 +1,6 @@
+""" leaving this here for reference, not going to use this code for now """
+
+from app import app
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy(app)
---
+++ gdelt-edu-web/run.py
@@ -0,0 +1,15 @@
+""" leaving this here for reference, not going to use this code for now """
+
+from app import app
+from db import db
+from models import freqused
+import routes
+import time
+from datetime import datetime
+
+
+with app.app_context():
+    # creates the tables defined in models
+    db.create_all()
+
+app.run()
